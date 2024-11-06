eab07f36f4ac7d00e2ef312dcadbf4d950b583e2
gwtovitor <gwtovitorpw@gmail.com>
2024-11-06 13:25:57 -0300

    add player jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
@@ -6,29 +6,43 @@ public class Player : MonoBehaviour
     public Rigidbody2D myRigidBody;
 
     [Header("Player Config")]
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(0.1f, 0);
     public float speed;
+    public float forceJump = 2;
 
     private void Update()
     {
-        PlayerMoviment();
+        HandleJump();
+        HandleMoviment();
     }
-    private void PlayerMoviment()
+    private void HandleMoviment()
     {
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            // myRigidBody.MovePosition(myRigidBody.position - velocity * Time.deltaTime);
             myRigidBody.velocity = new Vector2(-speed, myRigidBody.velocity.y);
 
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            //myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
             myRigidBody.velocity = new Vector2(speed, myRigidBody.velocity.y);
 
         }
 
+        if(myRigidBody.velocity.x > 0){
+            myRigidBody.velocity -= friction;
+        }else if(myRigidBody.velocity.x < 0){
+            myRigidBody.velocity += friction;
 
+        }
+
+    }
+    private void HandleJump()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.velocity = Vector2.up * forceJump   ;
+        }
     }
 }
