3fb370a17e7a67cfae1741c9e2b534bff7126840
gwtovitor <gwtovitorpw@gmail.com>
2024-11-12 13:23:10 -0300

    update

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -16,10 +16,17 @@ public class Player : MonoBehaviour
     [Header("AnimationSetup")]
     public float jumpScaleY = 1.5f;
     public float jumpScaleX = 0.7f;
-    public float impactScaleY = 0.8f; 
+    public float impactScaleY = 0.8f;
     public float impactScaleX = 1.2f;
     public Ease ease = Ease.OutBack;
     public float animationDuration = .3f;
+
+    [Header("Animation Player")]
+    public string boolRun = "Run";
+    public string boolFalling = "Falling";
+    public Animator animator;
+    public float playerSwipeDuration = .1f;
+
 
     private float _currentSpeed;
     private bool _isJumping = false;
@@ -28,26 +35,35 @@ public class Player : MonoBehaviour
     {
         HandleJump();
         HandleMoviment();
+        Runing();
     }
 
     private void HandleMoviment()
     {
-        if (Input.GetKey(KeyCode.LeftControl))
-        {
-            _currentSpeed = speedRun;
-        }
-        else
-        {
-            _currentSpeed = speed;
-        }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             myRigidBody.velocity = new Vector2(-_currentSpeed, myRigidBody.velocity.y);
+            if (myRigidBody.transform.localScale.x != -1)
+            {
+                myRigidBody.transform.DOScaleX(-1, playerSwipeDuration);
+            }
+            animator.SetBool(boolRun, true);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             myRigidBody.velocity = new Vector2(_currentSpeed, myRigidBody.velocity.y);
+            if (myRigidBody.transform.localScale.x != 1)
+            {
+                myRigidBody.transform.DOScaleX(1, playerSwipeDuration);
+            }
+
+            animator.SetBool(boolRun, true);
+        }
+        else
+        {
+            animator.SetBool(boolRun, false);
+
         }
 
         if (myRigidBody.velocity.x > 0)
@@ -58,6 +74,22 @@ public class Player : MonoBehaviour
         {
             myRigidBody.velocity += friction;
         }
+    }
+
+    private void Runing()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            _currentSpeed = speedRun;
+            animator.speed = 1.5f;
+        }
+        else
+        {
+            _currentSpeed = speed;
+            animator.speed = 1;
+
+        }
+
     }
 
     private void HandleJump()
@@ -74,23 +106,38 @@ public class Player : MonoBehaviour
 
     private void HandleJumpAnimate()
     {
-        myRigidBody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
-        myRigidBody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        // myRigidBody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        // myRigidBody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag(groundTag) && _isJumping)
+        if (collision.collider.CompareTag(groundTag))
         {
-            _isJumping = false;
-            DOTween.Kill(myRigidBody.transform);
-            HandleImpactAnimate();
+            animator.SetBool(boolFalling, false);
+
+
+            if (_isJumping)
+            {
+                _isJumping = false;
+                DOTween.Kill(myRigidBody.transform);
+                HandleImpactAnimate();
+            }
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag(groundTag))
+        {
+            animator.SetBool(boolFalling, true);
+
         }
     }
 
     private void HandleImpactAnimate()
     {
-        myRigidBody.transform.DOScaleY(impactScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
-        myRigidBody.transform.DOScaleX(impactScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        // myRigidBody.transform.DOScaleY(impactScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        // myRigidBody.transform.DOScaleX(impactScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
 }
